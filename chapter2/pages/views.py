694781a4a593b742565b19cde452bf2c81455ed5
KylesCorner <kylekrstulich@gmail.com>
2025-09-09 11:08:40 -0600

    moved chapter 2 to its own folder

---
+++ chapter2/pages/views.py
@@ -0,0 +1,9 @@
+# pages/views.py
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page_view(request):
+    return HttpResponse("Hello, World!")
+
+# Create your views here.
