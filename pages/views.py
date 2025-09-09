ba01e5cbf276d711a9591a8d9a9bf14bacf103a4
KylesCorner <kylekrstulich@gmail.com>
2025-09-09 11:10:40 -0600

    moved files

--- pages/views.py
+++
@@ -1,9 +0,0 @@
-# pages/views.py
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def home_page_view(request):
-    return HttpResponse("Hello, World!")
-
-# Create your views here.
